3e0e866d3cdc2310e1605595392c7c174a1d7d2c
NarendraPapasani <137531548+NarendraPapasani@users.noreply.github.com>
2024-08-13 20:55:23 +0530

    Update firebase.js
    removed firbase details

--- client/my-app/src/firebase/firebase.js
+++ client/my-app/src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyA4fbrj11SC4qyvCVEozYvpZVDWUFWP8ds",
-  authDomain: "react-blog-b5875.firebaseapp.com",
-  projectId: "react-blog-b5875",
-  storageBucket: "react-blog-b5875.appspot.com",
-  messagingSenderId: "55248785818",
-  appId: "1:55248785818:web:7f34a0741a3bb2833d2a23",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase

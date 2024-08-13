3153ddc60f5a3daad63d37ebced073315838a15b
NarendraPapasani <137531548+NarendraPapasani@users.noreply.github.com>
2024-08-13 20:56:19 +0530

    Update connectionDb.js
    removed sensitive details

--- server/config/connectionDb.js
+++ server/config/connectionDb.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://narendra22bce9801:VMuwiWg0RNbRfSGU@cluster0.3mcpxew.mongodb.net/profileData?retryWrites=true&w=majority&appName=Cluster0"
+      "your cluster connection uri"
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
